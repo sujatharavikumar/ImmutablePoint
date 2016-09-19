6050f74a9b9067cbb7178533ad4804cfa1aab726
Sujatha Ravikumar <sujatharavikumar@Sujatha-ZC-25.home>
2016-09-18 20:21:26 -0400

    First commit

---
+++ src/main/java/Ravikumar/Sujatha/ImmutablePoint/ImmutablePoint.java
@@ -0,0 +1,31 @@
+package Ravikumar.Sujatha.ImmutablePoint;
+
+/**
+ * Created by sujatharavikumar on 9/13/16.
+ */
+public class ImmutablePoint {
+    private int x,y,z;
+
+    public ImmutablePoint(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+
+}
+
+
+
